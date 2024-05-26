a79cf46e9ee4f8b9d76e263b6a69278a499f5c86
lilprincess19 <143635095+lilprincess19@users.noreply.github.com>
2024-05-26 08:04:16 -0500

    Learn git 2

---
+++ src/test/java/variousConcepts/LearnGit2.java
@@ -0,0 +1,17 @@
+package variousConcepts;
+
+public class LearnGit2 {
+	public static void main(String[] args) {
+
+	}
+
+	public void prinMe1() {
+		System.out.println("git test 1");
+	}
+	public void prinMe2() {
+		System.out.println("git test 1");
+}
+	public void prinMe3() {
+		System.out.println("git test 1");
+}
+}
